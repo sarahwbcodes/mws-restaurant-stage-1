f2fe52146417c19a00de890f54ce816e1a9baae7
Sarah <sarahwbcodes@gmail.com>
2018-05-22 06:11:29 +0300

    added ARIA label through js

--- js/main.js
+++ js/main.js
@@ -155,9 +155,10 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('a')
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute("aria-label", restaurant.name + " details");
   li.append(more)
 
   return li
