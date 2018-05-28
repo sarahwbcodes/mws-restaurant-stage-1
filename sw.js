7ca7b6c1408f893717b16141f2b5ac9d8eb56ec2
Sarah <sarahwbcodes@gmail.com>
2018-05-28 21:14:45 +0300

    added referencing and comments

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
+//Implementation of caching and installing is courtesy of Google Web Fundementals Service Worker Implementation
+//declaring variable version in case of versioning implementation
 var appCache = 'restaurant-app-cache-v1';
-var linksCached = [
+var appFilesCached = [
   '/',
   '/index.html',
   '/restaurant.html',
@@ -17,18 +19,34 @@ var linksCached = [
   '/img/10.jpg',
   '/js/dbhelper.js',
   '/js/main.js',
+  //the existence of 2 registration files is due to me experimenting with writing my own registration vs. Google's Web Fundementals prexisting implementation
+//the register_sw.js is Google's implementation and sw_registration.js is my implementation.
   '/js/register_sw.js',
   '/js/restaurant_info.js',
   '/js/sw_registration.js'
 ];
 
+  //this is the installation section
 self.addEventListener('install', function(event) {
-  // Perform install steps
   event.waitUntil(
     caches.open(appCache)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(linksCached);
+        return cache.addAll(appFilesCached);
       })
+  );
+});
+
+//fetches the cache
+self.addEventListener('fetch', function(event){
+  console.log(event.request.url);
+
+  event.respondWith(
+    cache.match(event.request)
+    .then(function(){
+      fetch(event.request);
+      //cloning for cache stability
+      event.request.clone();
+    })
   );
 });
